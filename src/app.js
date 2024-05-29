52b9728983ad2e7961912b8fc3383b9741498f0b
Jeff Rynhart <jeff.rynhart@du.edu>
2024-05-29 17:47:02 -0600

    Removed the static file path from the bootstrap file

--- src/app.js
+++ src/app.js
@@ -9,13 +9,6 @@ const app = express();
 const api = require('./api');
 const { notFound, errorHandler } = require('./middlewares/errors.middleware');
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/public'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  });
-}
-
 app.use(express.json());
 app.use(cors());
 
