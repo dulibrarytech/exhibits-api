585e3f34c5a66ff213d9324f4a46c5e8afcc7998
Jeff Rynhart <jeff.rynhart@du.edu>
2024-05-29 17:50:01 -0600

    Added runtime environment to startup messages

--- src/exhibits-api.js
+++ src/exhibits-api.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = process.env.PORT || 5678;
 
 app.listen(port, () => {
-  console.log(`Exhibits API is running on port http://localhost:${port}`);
+  console.log(`Exhibits API is running on port ${port} in ${process.env.NODE_ENV} mode`);
 });
