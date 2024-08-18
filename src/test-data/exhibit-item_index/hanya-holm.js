520340ec8bb6d3d354aeaaf5881612f87b465c2e
Jeff Rynhart <jeff.rynhart@du.edu>
2024-08-18 17:46:08 -0600

    updated hanya holm exhibit item data

--- src/test-data/exhibit-item_index/hanya-holm.js
+++ src/test-data/exhibit-item_index/hanya-holm.js
@@ -28,7 +28,7 @@ module.exports.exhibit_items = [
         "text": "<p>Hanya Holm was born in 1893 as Joanna Eckert in Worms, Germany. In the 1920s, Holm moved to Dresden to study with the German modern dance pioneer Mary Wigman, quickly becoming one of Wigman's most promising protégées. In the early 1930s, wary of Adolf Hitler's growing influence, Holm accepted dance impresario Sol Hurok's offer to run a Wigman school in the United States.</p><p>Though her school was based in New York, Holm traveled extensively to perform and teach. She first visited Colorado in 1933 when she taught at the Perry Mansfield Dance Camp in Steamboat Springs.</p><p>Holm's connection to the region became an enduring one. She began teaching an eight-week summer course at Colorado College in 1941, returning to the state every summer to teach for 43 years. Holm's yearly summer classes made Colorado a destination for modern dancers, and had an impact on dance and dancers in the state and region for decades.</p>",        
         "type": "item", 
         "layout": "media_left", 
-        "wrap_text": 0,
+        "wrap_text": 1,
         "media_width": "25",  
         "media_padding": 0,
         "styles": { 
@@ -49,7 +49,7 @@ module.exports.exhibit_items = [
         "text": "<p>Hanya Holm's dance career began in Germany, where she studied with, taught for, and eventually co-directed for Mary Wigman, the leading German modern dancer in the 1920s. Holm became Wigman's chief instructor in 1928.</p><p>Sol Hurok, a dance impresario who hoped to promote a Wigman company in America, offered to finance Holm’s move to the United States in 1931. Holm, who was unnerved by the rise of Adolf Hitler, eagerly accepted his offer. She would spend the early 1930s teaching the Wigman technique at her New York-based school, renaming it in 1936 (with Wigman's permission) to the Hanya Holm School of Dance.</p><p>These early years in America would influence Holm's move away from the darker, angst-ridden, European Wigman technique to a freer, more American style of modern dance.</p>",        
         "type": "item", 
         "layout": "media_right", 
-        "wrap_text": 0,
+        "wrap_text": 1,
         "media_width": "50",  
         "styles": { 
             backgroundColor: '#F0DDED' // light pink
@@ -86,7 +86,7 @@ module.exports.exhibit_items = [
         "text": "<p>Holm’s first visit to Colorado was in 1933, when she taught at the Perry Mansfield Dance Camp in Steamboat Springs, Colorado. This visit would prove to be significant - several of the students in that class (most significantly Martha Wilcox) would arrange, three years later, for the Denver debut of Holm's company. Hanya's Colorado compatriots also played an important role in the early success of her summer dance school at Colorado College.</p><p>These early connections were the beginning of lifelong relationship with the West. After 1941, Holm would spend the next forty-three summers teaching, working, and living in Colorado. Her summer dance school made the state a destination for anyone who wanted to learn about modern dance. Her influence on performing arts in the region as an educator, mentor, and friend enabled future generations of dancers, choreographers, and educators to find their own voices and learn to tell their own stories.</p>",        
         "type": "item", 
         "layout": "media_top", 
-        "wrap_text": 0,
+        "wrap_text": 1,
         "media_width": "100",  
         "styles": { 
             backgroundColor: '#FFFFFF' 
@@ -121,7 +121,7 @@ module.exports.exhibit_items = [
         "text": "<p>Hanya Holm’s career was notable for its constant evolution and her experimentation – the only constant was change. She believed that all choreography should evolve organically out of an understanding of the work itself and from a natural movement base. This point of view enabled her to transition seamlessly between different styles, genres, and mediums.</p><p>Over the course of her career, Holm choreographed for the concert stage, television, Broadway musicals, movies, and opera. She was also notable for being the first to copyright a choreographic work when she submitted the Labanotation score for Kiss Me Kate to the Library of Congress.</p><p>Holm's chameleon-like quality as a choreographer ultimately benefitted the work - however, dance scholar Claudia Gitelman has asserted that Holm's focus on serving the project rather than her \"brand\" has led to a lack of recognition relative to the other \"big four\" modern dance pioneers.</p>",        
         "type": "item", 
         "layout": "media_left", 
-        "wrap_text": 0,
+        "wrap_text": 1,
         "media_width": "25",  
         "media_padding": 0,
         "styles": { 
@@ -142,7 +142,7 @@ module.exports.exhibit_items = [
         "text": "<p>Holm’s first choreographic works were in the realm of modern dance, and reflected her training with the German expressionist pioneer Mary Wigman, with whom she had studied and worked with since the early 1920s.</p><p>Trend (1937), Holm’s best-known modern dance work, received the New York Times’ award for being the best dance composition of the year. Metropolitan Daily, Holm’s satiric look at newspapers, was the first modern dance work to be performed on television in 1938, and Dance of Work and Play (1938), Tragic Exodus (1939), and They Too Are Exiles (1939) soon followed.</p><p>These works solidified Holm’s reputation as one of the most innovative modern dance choreographers of the era.</p>",        
         "type": "item", 
         "layout": "media_right", 
-        "wrap_text": 0,
+        "wrap_text": 1,
         "media_width": "50",  
         "styles": { 
             backgroundColor: '#F8FAE4' // light yellow
@@ -247,7 +247,7 @@ module.exports.exhibit_items = [
         "text": "<p>Hanya Holm’s background and training was in modern dance, but her approach to teaching dance mirrored her genre-agnostic approach to choreography – and because of it, her influence extended far beyond the modern dance community.</p><p>Holm began teaching in Germany for expressionist dance pioneer Mary Wigman, and then founded a Wigman School in New York, which was soon renamed the Hanya Holm School of Dance. Starting in 1934, she taught at the now-internationally renowned Bennington School of Dance at Bennington College in Vermont. Bennington was also home to the other “Big Four” modern dance pioneers in America – Charles Weidman, Doris Humphrey, and Martha Graham.</p><p>In 1941, Holm found her “teaching home” at Colorado College, which she would make her summer destination for 43 years. She continued to work internationally on other projects throughout those years, but for those who attended Holm’s eight-week summer dance school in Colorado, her most lasting impact on the dance world was as an educator.</p>",
         "type": "item", 
         "layout": "media_left", 
-        "wrap_text": 0,
+        "wrap_text": 1,
         "media_width": "25",  
         "media_padding": 0,
         "styles": { 
@@ -268,7 +268,7 @@ module.exports.exhibit_items = [
         "text": "<p>In 1934, soon after Holm came to the United States to direct the Wigman School, she began teaching summer modern dance classes at the prestigious Bennington School of Dance. Some of the other teachers included Martha Graham, Charles Weidman, and Doris Humphrey, all of whom are now known as the “Big Four” in modern dance.</p><p>During her time at Bennington, Holm was a founding member of the Bennington Festival, which later became the American Dance Festival. The festival provided a venue for these modern dance educators to experiment and train students at the same time, which led to some of Holm's best known modern dance works, such as Trend (1937).</p><p>Maxine Munt, a company member who later founded the Changing Scene Theatre in Denver with husband and fellow company member Al Brooks, documented her time at Bennington in a scrapbookof photographs that she donated to the University of Denver. Munt's photographs provide an intimate and personal depiction of what life was like for students who were fortunate enough to study and perform at Bennington.</p>",        
         "type": "item", 
         "layout": "media_bottom", 
-        "wrap_text": 0,
+        "wrap_text": 1,
         "media_width": "100",  
         "styles": { 
             backgroundColor: '#E8DFEB' // light violet 
@@ -484,9 +484,141 @@ module.exports.exhibit_items = [
         "is_published": 0
     }, 
 
+    {
+        "uuid": "551e72040bba049c3fc6b88e27cf0396",
+        "is_member_of_exhibit": "642bbd319b780725f753b15e1df520e6",
+        "title": "<span style='color: #6C317D'>Colorado College</span>", 
+        "caption": "Hanya Holm speaks to students (among them Martha Wilcox and Vera Sears) at Colorado College, between 1943-1944", 
+        "template": "row", 
+        "item_type": "image", 
+        "media": "551e72040bba049c3fc6b88e27cf0396.jpg",
+        "text": "<p>In 1941, Holm moved her teaching to Colorado College, which would become her summer home for another 43 years, a connection that made Colorado a destination for modern dance teaching. Over those years, she taught Freidann Parker and Lillian Covillo (co-founders of Colorado Ballet), Vera Sears (director of the Dance Department at the Lamont School of Music, University of Denver), Alfred Brooks and Maxine Munt (co-founders of the Changing Scene Theatre in Denver), and many more.</p><p>Holm was a strict taskmaster – one student noted that “those with a strongly built, stable ego could get the most out of working with her...Hanya’s reactions were often hard to take by those whose ways were not set.” However, if you could learn from Holm’s criticism, as Valerie Bettis, one of the original members of Holm’s dance company, told the New York Times, you had \"a foundation to move in any direction [you] liked.\"</p>",        
+        "type": "item", 
+        "layout": "media_right", 
+        "wrap_text": 1,
+        "media_width": "50",  
+        "styles": { 
+            backgroundColor: '#E8DFEB' 
+        },
+        "is_published": 0, 
+        "order": 17
+    },
+
+    {
+        "uuid": "813f9bf984800fb2f570baeecff23fd3",
+        "is_member_of_exhibit": "642bbd319b780725f753b15e1df520e6",
+        "caption": "Hanya Holm receives an honorary degree from Colorado College, 1960", 
+        "template": "row", 
+        "item_type": "image", 
+        "media": "813f9bf984800fb2f570baeecff23fd3.jpg",
+        "text": "Holm earned an honorary degree in 1960, and Colorado College held multiple festivals over the years to honor Holm's legacy, all of which were attended by former students and assistants. After attending one such festival, Dance Magazine's Nik Krevitsky wrote \"the evening was filled with convincing evidence that Hanya Holm...has prepared her students...to become independent artists and to develop the security of their own uniqueness.\"",        
+        "type": "item", 
+        "layout": "media_left", 
+        "wrap_text": 1,
+        "media_width": "50",  
+        "styles": { 
+            backgroundColor: '#E8DFEB' 
+        },
+        "is_published": 0, 
+        "order": 18
+    },
+
     //////////////////
     // end ingested
     //////////////////
+
+    {
+        "uuid": "ea3b6ba68114b3514b71a48757093e51",
+        "type": "heading",
+        "is_member_of_exhibit": "642bbd319b780725f753b15e1df520e6", 
+        "text": "Hanya as Mentor",
+        "styles": {
+            backgroundColor: '#D68037', // orange
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 19, 
+    },
+
+    {
+        "uuid": "34a00ddb80b5773e38e97d4520939522",
+        "is_member_of_exhibit": "642bbd319b780725f753b15e1df520e6",
+        "template": "row", 
+        "item_type": "image", 
+        "media": "34a00ddb80b5773e38e97d4520939522.jpg",
+        "text": "<p>Hanya Holm’s gift as a mentor was, as dance critic Walter Terry noted, not just to teach movement but to teach \"the essence and the spirit of dancing.\"</p><p>Some of Holm’s best-known students (Alwin Nikolais, Valerie Bettis, Louise Kloepper, and Don Redlich) worked primarily in modern dance, but Holm’s influence extended into both ballet and theater. The Colorado Ballet was founded by Lillian Covillo and Freidann Parker, both former students of Holm’s. Martha Wilcox, Vera Sears, and Carolyn Durand Brooks, all former students or Holm company members, worked in or with the Children’s Dance Theatre at the University of Denver’s Lamont School of Music. Al Brooks, former student and company member, and Maxine Munt, a former student, met, married, and started the Changing Scene Theatre in Denver in 1968.</p><p>Walter Terry noted that Holm’s commitment to not just one style of dance, but to the art of dance itself, uniquely placed her to teach individuals \"not to become modern dancers...but how to be dancers.\”</p>",        
+        "type": "item", 
+        "layout": "media_left", 
+        "wrap_text": 1,
+        "media_width": "25",  
+        "media_padding": 0,
+        "styles": { 
+            backgroundColor: '#FFFFFF'
+        },
+        "is_published": 0, 
+        "order": 20
+    },
+
+    {
+        "uuid": "28314325314843dcbf419a82d7b18c72",
+        "is_member_of_exhibit": "642bbd319b780725f753b15e1df520e6",
+        "title": "Colorado Ballet", 
+        "caption": "Freidann Parker poses at Red Rocks Amphitheater, circa 1950", 
+        "template": "row", 
+        "item_type": "image", 
+        "media": "28314325314843dcbf419a82d7b18c72.jpg",       
+        "type": "item", 
+        "layout": "media_only", 
+        "wrap_text": 1,
+        "media_width": "100",  
+        "styles": { 
+            backgroundColor: '#F8EDDF' // light orange
+        },
+        "is_published": 0, 
+        "order": 21
+    },
+
+    {
+        "uuid": "754e4e59f72c4f84b8dd05e14c9c6f70",
+        "is_member_of_exhibit": "642bbd319b780725f753b15e1df520e6",
+        "caption": "Vera Sears, Freidann Parker, Hanya Holm, and Lillian Covillo pose outside the Colorado Ballet Center, 1978", 
+        "template": "row", 
+        "item_type": "image", 
+        "text": "<p>In the early 1940s, Lillian Covillo (co-founder of Colorado Ballet) had an impulse to try something a little different. A ballet student since the age of nine, Covillo recalled thinking \"if I don’t get out of fifth position, I’m going to scream! I need to move!\”</p><p>This impulse drew Covillo to study modern dance at the University of Denver. There, she met the free-spirited Freidann Parker and a lifelong partnership was born. Parker had studied with Hanya Holm in New York, and, according to Covillo, had modern dance \"in her bones.\" The two decided to put their respective talents together to create, as Covillo put it, a \"more eclectic sort of ballet company that would do modern.\"</p><p>Covillo credited Holm with teaching her and Parker a system of choreography that they used throughout the life of Colorado Ballet. Reflecting on Holm’s influence on choreography, Covillo said: \"I look at things now...and I think, ‘Hanya would collapse if she saw this!’ It has no form, it has no pattern – all the things that are very essential in choreography....Hanya had an absolute pattern.\"</p>", 
+        "media": "754e4e59f72c4f84b8dd05e14c9c6f70.jpg",
+        "type": "item", 
+        "layout": "media_right", 
+        "wrap_text": 1,
+        "media_width": "50",  
+        "styles": { 
+            backgroundColor: '#F8EDDF' // light orange
+        },
+        "is_published": 0, 
+        "order": 22
+    },
+
+    {
+        "uuid": "a0494efce4628c9f09cb150359f03183",
+        "is_member_of_exhibit": "642bbd319b780725f753b15e1df520e6",
+        "title": "Children's Dance Theatre", 
+        "caption": "Vera Sears teaches a children's dance class at the Lamont School of Music mansion, 909 Grant Street", 
+        "template": "row", 
+        "item_type": "image", 
+        "media": "a0494efce4628c9f09cb150359f03183.jpg",
+        "text": "<p>Many of Hanya Holm’s former students and company members settled in Colorado. Several of these went on to both teach and direct at the Children’s Dance Theatre, which was a part of the University of Denver's Lamont School of Music's Dance Department. These students included Freidann Parker, later co-founder of the Colorado Ballet, and Carolyn Durand Brooks, an early company member and longtime friend of Holm.</p><p>Martha Wilcox and Vera Sears, also former students, both taught and served as directors. Sears, who directed the Children’s Dance Theatre for 17 years, called teaching children her \"life’s work,\" and in her oral history, conducted in 2004, said that \"the most important part of [her] life was the Children's Dance Theatre.\"</p><p>The Lamont School of Music was founded in 1924 by noted voice teacher Florence Lamont Hinman, but functioned as a private, proprietary music school until its merger with the University of Denver in 1941. From 1941 through at least 1958, the Children’s Dance Theatre, which was part of Lamont’s Dance Department, operated out of a mansion at 909 Grant Street. Allen Young, music critic for the Denver Post, said about the Children’s Dance Theatre: \"Children and dancers express themselves in distinctive terms, and when youthful dancers turn and leap with natural exuberance...it is a sight hard to resist.\"</p>",        
+        "type": "item", 
+        "layout": "media_left", 
+        "wrap_text": 1,
+        "media_width": "50",  
+        "styles": { 
+            backgroundColor: '#FFFFFF' 
+        },
+        "is_published": 0, 
+        "order": 23
+    },
 
     ///////////////////
     // template objects
