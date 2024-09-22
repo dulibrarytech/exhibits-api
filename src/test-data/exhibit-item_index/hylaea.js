acb04c512cb9d667ef889be39cf44e5b926b4f1a
Jeff Rynhart <jeff.rynhart@du.edu>
2024-09-22 17:13:33 -0600

    completed hylaea exhibit item data

---
+++ src/test-data/exhibit-item_index/hylaea.js
@@ -0,0 +1,431 @@
+module.exports.exhibit_items = [
+    {
+        "uuid": "b9c0c11e55cfac6c7064aecc6eb4f838",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "The Name",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 1, 
+    },
+
+    {
+        "uuid": "5f0dd51c6aec0654a649664c0d6c091c",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "item_type": "text",
+        "text": "HYLAEA, the installation’s title, refers to the mythical forests of ancient Greece, which 18th century German naturalist Alexander von Humboldt claimed he rediscovered during his exploration of South America’s Amazon rainforests. The interplay between mythical and historical notions of HYLAEA resonates with contemporary worries about human-induced environmental destruction on today’s Amazon rainforests.",
+        "type": "item",
+        "layout": "text_only",
+        "styles": {
+            backgroundColor: '#CCCBCD',
+        },
+        "is_published": 0,
+        "order": 2
+    },
+
+    {
+        "uuid": "6e15b166171589e00bd3a3f0c13fba48",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "The Experience",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 3, 
+    },
+
+    // {
+    //     "uuid": "0c83c8a80e5054d101a3f4594ad88006",
+    //     "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+    //     "item_type": "external",
+    //     "media": "https://www.youtube.com/embed/_fzIE1kZ_c0",
+    //     "text": "<i>“[HYLAEA is] an interactive video and print installation that seeks to bring to life the residues, records, and archives of lost ecological memory. Part ecological ghost story/part zoological bibliography, this project stems from a desire to partially awaken the resting memories of lost life forms from the extinct species cabinets of the museum and the rare book shelves of the library. My intention in seeding motion, sound, interaction and macroscopic detail across the library is for viewers to recall that within less than a century of the publication of the first written and painted records of the astounding bird life of North America, both common and mythological species became the icons of human-induced extinctions.” (Tim Weaver, 2010)</i>",
+    //     "wrap_text": 1,
+    //     "type": "item",
+    //     "layout": "media_top",
+    //     "media_width": "100",
+    //     "media_padding": 1,
+    //     "styles": {
+    //         backgroundColor: '#FFFFFF',
+    //     },
+    //     "is_published": 0,
+    //     "is_embedded": 1,
+    //     "order": 4
+    // },
+
+    {
+        "uuid": "0c83c8a80e5054d101a3f4594ad88006",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "item_type": "video",
+        "media": "",
+        "kaltura_id": "0_unc0v4as",
+        "text": "<i>“[HYLAEA is] an interactive video and print installation that seeks to bring to life the residues, records, and archives of lost ecological memory. Part ecological ghost story/part zoological bibliography, this project stems from a desire to partially awaken the resting memories of lost life forms from the extinct species cabinets of the museum and the rare book shelves of the library. My intention in seeding motion, sound, interaction and macroscopic detail across the library is for viewers to recall that within less than a century of the publication of the first written and painted records of the astounding bird life of North America, both common and mythological species became the icons of human-induced extinctions.” (Tim Weaver, 2010)</i>",
+        "wrap_text": 1,
+        "type": "item",
+        "layout": "media_top",
+        "media_width": "100",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#FFFFFF',
+        },
+        "is_published": 0,
+        "is_embedded": 1,
+        "order": 4
+    },
+
+    {
+        "uuid": "29015d1b3b91459cad1cf58d0a9e4da4",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "The Process",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 5, 
+    },
+
+    {
+        "uuid": "6780f71efaff2c434fd31a9f7799e41e",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "item_type": "video",
+        "media": "",
+        "kaltura_id": "0_unc0v4as",
+        "text": "This time lapse video shows the installation of an assemblage print on the main level of the old library building. The other exhibits located throughout the library included a looped video projection with background lights and sounds located in the main stairwell, an interactive light and sound projection in the upper level book stacks, framed prints and book displays on the main level, and additional assemblage prints in the northwest corner of the upper level.",
+        "wrap_text": 1,
+        "type": "item",
+        "layout": "media_bottom",
+        "media_width": "100",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#CCCBCD',
+        },
+        "is_published": 0,
+        "is_embedded": 1,
+        "order": 6
+    },
+
+    {
+        "uuid": "269bf86fa5432370884447b01375c9b3",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "Birds in the Stairwell",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 7, 
+    },
+
+    {
+        "uuid": "a371474e02e855ba46f6f0a529e1a692",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "caption": "",
+        "item_type": "image",
+        "media": "a371474e02e855ba46f6f0a529e1a692.jpg",
+        "text": "<div><p>In the cycling projected video loop, Weaver paged through an original edition of John James Audubon’s <i>The Birds of America: From Drawings Made in the United States and their Territories</i> from University Libraries' Special Collections.</p></div><div style=\"width: 80%; margin: 50px auto 0 auto\"><a href=\"#\"><button style=\"background-color: #B5CC5B; border-radius: 20px; padding: 15px; color: white; font-weight: bold;\">VIEW THIS BOOK IN THE UNIVERSITY LIBRARIES COLLECTION</button></a></div>",
+        "wrap_text": 0,
+        "type": "item",
+        "layout": "media_left",
+        "media_width": "66",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#FFFFFF',
+        },
+        "is_published": 0,
+        "order": 8
+    },
+    
+    {
+        "uuid": "26372155203510647e34e7bb0b87975b",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "caption": "",
+        "item_type": "image",
+        "media": "26372155203510647e34e7bb0b87975b.jpg",
+        "text": "<div><p>During the video, there is a notable hesitation in Weaver’s paging through <i>Birds of America</i> when he encountered images of the extinct Ivory-billed Woodpecker, Passenger Pigeon, and Carolina Parakeet along with the endangered Whooping Crane.</p></div><div style=\"width: 80%; margin: 50px auto 0 auto\"><a href=\"#\"><button style=\"background-color: #B5CC5B; border-radius: 20px; padding: 15px; color: white; font-weight: bold;\">VIEW CONTENT FROM BIRDS OF AMERICA AT AUDUBON.ORG</button></a></div>",
+        "wrap_text": 0,
+        "type": "item",
+        "layout": "media_left",
+        "media_width": "66",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#FFFFFF',
+        },
+        "is_published": 0,
+        "order": 9
+    },
+
+    {
+        "uuid": "47e91c2723af2f5f1486f930753cb832",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "Books and Birds",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 10, 
+    },
+
+    {
+        "uuid": "cc941b2bbb364c8a9be1028bcaf13ef3",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "caption": "",
+        "item_type": "image",
+        "media": "cc941b2bbb364c8a9be1028bcaf13ef3.jpg",
+        "text": "<p>This dual display featured framed, digital images of the Passenger Pigeon, Ivory-billed Woodpecker, Bachman’s Warbler, Eskimo Curlew, Carolina Parakeet and Imperial Woodpecker, from the Denver Museum of Nature and Science’s extinct bird collection. The framed prints are interspersed with enlarged pictures from John James Audubon’s <i>The Birds of America: From Drawings Made in the United States and their Territories</i>, original print editions of Audubon’s <i>The Birds of America</i> and <i>Quadrupeds of North America</i>, and Carl Lumholtz’s <i>Unknown Mexico: A Record of Five Years’ Exploration Among the Tribes of the Western Sierra Madre; In the Tierra Caliente of Tepic and Jalisco; And Among the Tarascos of Michoacan</i>.</p>",
+        "wrap_text": 1,
+        "type": "item",
+        "layout": "media_top",
+        "media_width": "100",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#CCCBCD',
+        },
+        "is_published": 0,
+        "order": 11
+    },
+
+    {
+        "uuid": "9d5d1072d7b44ee008dd3e89942f8bc4",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "Sounds in the Stacks",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 12, 
+    },
+
+    {
+        "uuid": "4a78fea86613e1e374deb5429ecaf733",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "caption": "",
+        "item_type": "image",
+        "media": "4a78fea86613e1e374deb5429ecaf733.jpg",
+        "text": "<p>Weaver played with interaction and interactivity by linking two of his instalaltions in the library space. In <i>Birds in the Stairwell</i>, when Weaver paused his page turning on extinct or threatened birds, the video display located in the stacks simultaneously showed details of lost species coming into focus, while audio of a single bird chirp gave way to many cries from the surviving members of birds related to these extinct species.</p>",
+        "wrap_text": 1,
+        "type": "item",
+        "layout": "media_left",
+        "media_width": "66",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#FFFFFF',
+        },
+        "is_published": 0,
+        "order": 13
+    },
+
+    {
+        "uuid": "7dac3ced3fd0b0ee3de9e71332e6dc0b",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "caption": "",
+        "item_type": "image",
+        "media": "7dac3ced3fd0b0ee3de9e71332e6dc0b.jpg",
+        "text": "<p>Once paging resumed, the <i>Sounds in the Stacks</i> installation displayed an overview of lost habitat synced with computer-generated audio based on human cryptochromic or light-sensitive proteins. Weaver suggested that these proteins sense time and light and may spur the human drive for territoriality.</p>",
+        "wrap_text": 1,
+        "type": "item",
+        "layout": "media_left",
+        "media_width": "66",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#FFFFFF',
+        },
+        "is_published": 0,
+        "order": 14
+    },
+
+    {
+        "uuid": "8690462c67ab55060d8d566b0b06773b",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "Birds Above the Campus",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 15, 
+    },
+
+    {
+        "uuid": "5c1fe9241db559d8560c21d6aba4713c",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "caption": "",
+        "item_type": "image",
+        "media": "5c1fe9241db559d8560c21d6aba4713c.jpg",
+        "text": "<p>High-resolution, large scale assemblage prints created from the Denver Museum of Nature and Science’s extinct bird collection lined the walls of the reading area on the upper level. Weaver placed prints of the Eskimo Curlew, Carolina Parakeet, and Imperial Woodpecker between the library’s large windows in order to integrate them with the campus’ visual space.</p><p>The juxtaposition of these extinct species against the University of Denver's vibrant outdoor landscape heightens the dichotomy between the natural world as it once existed and how we experience it today.</p>",
+        "wrap_text": 1,
+        "type": "item",
+        "layout": "media_left",
+        "media_width": "66",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#CCCBCD',
+        },
+        "is_published": 0,
+        "order": 16
+    },
+
+    {
+        "uuid": "cef6355356386250e33f264664dcdace",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "caption": "",
+        "item_type": "image",
+        "media": "cef6355356386250e33f264664dcdace.jpg",
+        "type": "item",
+        "layout": "media_only",
+        "media_width": "100",
+        "media_padding": 1,
+        "styles": {
+            backgroundColor: '#CCCBCD',
+        },
+        "is_published": 0,
+        "order": 17
+    },
+
+    {
+        "uuid": "85d6172f1cc4a7bdbf3c39f6311bdc1c",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "Artist Bio",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 18, 
+    },
+
+    {
+        "uuid": "bde58a87ff8af0ed104506f70c11893b",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "item_type": "text",
+        "text": "<div><p>High-resolution, large scale assemblage prints created from the Denver Museum of Nature and Science’s extinct bird collection lined the walls of the reading area on the upper level. Weaver placed prints of the Eskimo Curlew, Carolina Parakeet, and Imperial Woodpecker between the library’s large windows in order to integrate them with the campus’ visual space.</p><p>Dr. Timothy Weaver is a new media artist and former life scientist whose ambition is to contribute to the restoration of ecological memory through a process of speculative inquiry along the art/technology interface. Weaver’s live cinema, video and sound-art projects, and interactive installations have been featured at the Denver Art Museum, FILE Hipersonica (Brazil), Transmediale (Berlin), SubtleTechnologies Festival (Toronto), New Forms Festival (Vancouver), Korean Experimental Art Festival, Boston CyberArts, SIGGRAPH, and the New York Digital Salon. Weaver is currently an associate professor and director of Emergent Digital Practices (EDP) in the School of Art and Art History at the University of Denver.</p><p>The juxtaposition of these extinct species against the University of Denver's vibrant outdoor landscape heightens the dichotomy between the natural world as it once existed and how we experience it today.</p></div><div style=\"margin-top: 50px\"><a href=\"#\"><button style=\"background-color: #B5CC5B; border-radius: 20px; padding: 15px; color: white; font-weight: bold;\">MORE INFORMATION ABOUT THE ARTIST</button></a></div>",
+        "type": "item",
+        "layout": "text_only",
+        "styles": {
+            backgroundColor: '#FFFFFF',
+        },
+        "is_published": 0,
+        "order": 19
+    },
+
+    //////////////////////
+    // end ingested items
+    //////////////////////
+
+    ///////////////////
+    // template objects
+    ///////////////////
+
+    {
+        "uuid": "",
+        "type": "heading",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93", 
+        "text": "",
+        "styles": {
+            backgroundColor: '#333333',
+            color: '#FFFFFF',
+            fontSize: "47px"
+        },
+        "is_visible": 1, 
+        "is_anchor": 1,
+        "is_published": 0,
+        "order": 0, 
+    },
+
+    {
+        "uuid": "",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "caption": "",
+        "item_type": "image",
+        "media": "",
+        "text": "",
+        "wrap_text": 1,
+        "type": "item",
+        "layout": "media_right",
+        "media_width": "50",
+        "media_padding": 1,
+        "styles": {},
+        "is_published": 0,
+        "order": 0
+    },
+
+    { 
+        "uuid": "",
+        "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+        "title": "",
+        "type": "grid",
+        "columns": "2",
+        "order": 0,
+        "styles": {},
+        "items": [
+            {
+                "uuid": "",
+                "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+                "item_type": "image", 
+                "media": "", 
+                "description": "",        
+                "type": "item", 
+                "styles": {},
+                "is_published": 1, 
+                "order": 1
+            },
+            {
+                "uuid": "",
+                "is_member_of_exhibit": "811c7fc9ed4f91b837b4f49ae8901c93",
+                "item_type": "image", 
+                "media": "", 
+                "description": "",        
+                "type": "item", 
+                "styles": {},
+                "is_published": 1, 
+                "order": 2
+            }
+        ],
+        "is_published": 0
+    },
+]
