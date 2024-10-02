9ca59244e725a575c95f10d7f4ac4f460e5db1a8
Jeff Rynhart <jeff.rynhart@du.edu>
2024-10-02 09:38:01 -0600

    removed vertical timeline data from exhibit item data examples

--- example-exhibit-item-data-objects.js
+++ example-exhibit-item-data-objects.js
@@ -65,52 +65,6 @@ let example_exhibit_item_objects = [
         "items": [
             // grid item fields (contains all item fields. grid specific fields are shown below) :
             {
-                "description": "", // {string | html} description text displayed on the grid item. *IF there is no description, but there is text, the text will be displayed on the grid item and ALSO in the modal viewer.
-                "uuid": "{item uuid}",
-                "is_member_of_exhibit": "{parent exhibit uuid}",
-                "thumbnail": "{filename.ext}", // path to user uploaded thumbnail image, or url to external thumbnail image
-                "title": "Item Title", // {string} (default: null, item displays no title) *** The title field will appear in the navigation as a sublink under the previous page heading ***
-                "caption": "Item caption text", // {string} (default: null, item displays no caption under media content)
-                "item_type": "image", // {'image', 'audio', 'video', 'pdf', 'external'} (R)
-                "media": "{filename or ddu pid}", // { {filename}.{extension} OR {digitaldu item uuid} (IF 'is_repo_item' == true) } (R if no "text" value)
-                "text": "", // {string | html} 
-                "type": "item", // {'row' | 'grid' | 'vertical_timeline' | 'heading'} (R)
-                "layout": "media_left", // {'media_right' | 'media_left' | 'media_top' | 'media_bottom' | "media_only" | "text_only"} (R)
-                "media_width": "50", // {25|33|50|66|75} width of the media element in the item as percent (default: '50') * use only on side-by-side layouts 'media_right' and 'media_left' 
-                "media_padding": 1, // bool {0,1} If 1, margins will be added to the item display. If 0, there will be no margins (default: 1)
-
-                /* user style settings (default: {}) */
-                "styles": { 
-                    "backgroundColor": "", // hex or rgb value from color picker
-                    "color": "", // hex or rgb value from color picker
-                    "fontFamily": "", // list of font-family options
-                    "fontSize": ""
-                },
-            
-                "is_repo_item": 0, // bool {0,1} If 1, will use the digitaldu object uuid in the "media" field above to stream the item from the repository (default: 0)
-                "pdf_open_to_page": 1, // {integer} For 'item_type': 'pdf', will open the pdf file to this page (default: 1)  
-                "is_published": 1, // (default: 0)
-                "order": 1
-            }
-        ],
-        "is_published": 1
-    },
-    
-    // vertical timeline item grid (same as item grid - with additional "year_label" property in the grid items)
-    {
-        "uuid": "{grid item uuid}",
-        "is_member_of_exhibit": "{parent exhibit uuid}",
-        "type": "vertical_timeline", // (R)
-        "title": "Item Grid Title", // *** The title field will appear in the navigation as a sublink under the previous page heading ***
-        "order": 1,
-        "styles": {
-            "color": "#303030",
-            "backgroundColor": "gray",
-            "fontFamily": "Cursive"
-        },
-        "items": [
-             // grid item fields (contains all item fields. grid specific fields are shown below) :
-            {
                 "date": "", // {string} date string e.g. '2022-10-13T20:24:20.000Z'
                 "description": "", // {string | html} description text displayed on the grid item. *IF there is no description, but there is text, the text will be displayed on the grid item and ALSO in the modal viewer.
                 "uuid": "{item uuid}",
@@ -141,4 +95,5 @@ let example_exhibit_item_objects = [
             }
         ],
         "is_published": 1
-    }]
+    }
+]
