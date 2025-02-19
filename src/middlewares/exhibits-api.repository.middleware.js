5a9dc1811446495151908ab527691856b9f8d68b
Jeff Rynhart <jeff.rynhart@du.edu>
2025-02-19 10:00:59 -0700

    updated repository api middleware request validator to require file extensions are within a set of allowed values on resource/fetch route

--- src/middlewares/exhibits-api.repository.middleware.js
+++ src/middlewares/exhibits-api.repository.middleware.js
@@ -8,12 +8,15 @@ const getDataValidator = async (req, res, next) => {
 }
 
 const fetchResourceFileValidator = async (req, res, next) => {
+    const validFileExtensions = ['png', 'jpg', 'jpeg', 'tif', 'tiff', 'mp3', 'mp4'];
+
     validate(req, res, next, [
         param('id').isLength({min: 1, max: 50}).withMessage("id param length range: 1-50 characters"),
         body('exhibitItemId').notEmpty(),
         body('fileExtension').notEmpty(),
         body('exhibitItemId').isLength({min: 1, max: 50}).withMessage("exhibitItemId value length range: 1-50 characters"),
-        body('fileExtension').isLength({min: 3, max: 4}).withMessage("fileExtension value length range: 3-4 characters")
+        body('fileExtension').isLength({min: 3, max: 4}).withMessage("fileExtension value length range: 3-4 characters"),
+        body('fileExtension').isIn(validFileExtensions).withMessage(`Valid file extensions: ${validFileExtensions.toString()}`),
     ]);
 }
   
