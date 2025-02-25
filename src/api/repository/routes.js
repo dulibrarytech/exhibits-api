71316fb978d5147088810083426e3f0b762b75b2
Jeff Rynhart <jeff.rynhart@du.edu>
2025-02-24 17:36:37 -0700

    removed api key verification via url parameter from /repository routes

--- src/api/repository/routes.js
+++ src/api/repository/routes.js
@@ -7,12 +7,8 @@
 const ROUTER = require('express').Router();
 const CONTROLLER = require('./controller');
 
-const { validateApiKey } = require('../../middlewares/exhibits-api.authentication.middleware');
 const { fetchResourceFileValidator, getDataValidator } = require('../../middlewares/exhibits-api.repository.middleware');
 
-/////////////////
-// public routes
-/////////////////
 ROUTER.get('/data/:id', getDataValidator, (req, res) => {
   CONTROLLER.getData(req, res);
 });
@@ -20,11 +16,6 @@ ROUTER.get('/data/:id', getDataValidator, (req, res) => {
 ROUTER.post('/search', (req, res) => {
   CONTROLLER.search(req, res);
 });
-
-////////////////////
-// protected routes
-////////////////////
-ROUTER.use('/resource/fetch', validateApiKey);
 
 ROUTER.post('/resource/fetch/:id', fetchResourceFileValidator, async (req, res) => {  
   CONTROLLER.fetchResourceFile(req, res);
