5e14b7ffbb0602b3ce883d6b1c495b8b0d37d38f
Jeff Rynhart <jeff.rynhart@du.edu>
2025-07-10 20:22:20 -0600

    updated searchIndex middleware validation to display all valid search types (exhibit, item search type values) in validator error message

--- src/middlewares/exhibits-api.search.middleware.js
+++ src/middlewares/exhibits-api.search.middleware.js
@@ -8,7 +8,7 @@ const searchIndexValidator = async (req, res, next) => {
         query('q').notEmpty(),
         query('q').isLength({min: 1, max: 50}).withMessage("q length range: 1-50 characters"),
         query('page').isInt({ min: 1, max: 100 }).withMessage("page must be an integer between 1 and 100"),
-        query('type').isIn(validTypes).withMessage("valid type values: 'item', 'exhibit'"),
+        query('type').isIn(validTypes).withMessage(`valid type values: ${validTypes.toString()}`),
         query('exhibitId').isLength({ max: 50 }).withMessage("exhibitId length range: 1-50 characters")
     ]);
 }
