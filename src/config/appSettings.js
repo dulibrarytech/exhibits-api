e1c6b3b261418ef5d01b936a4132ad8706b317d0
Jeff Rynhart <jeff.rynhart@du.edu>
2026-06-03 17:34:44 -0600

    added new settings module added repository iiif urls to settings
    ref #486d0ae3eff3f4f261e7034182900df738df74ad
    github #21

---
+++ src/config/appSettings.js
@@ -0,0 +1,22 @@
+// appSettings.js
+
+'use strict'
+
+module.exports = {
+    app: {
+      appTitle: "Exhibits App",
+      appDescription: "A digital exhibit builder for libraries and museums.",
+      appKeywords: "digital exhibits, library exhibits, museum exhibits, exhibit builder",
+      appAuthor: "University of Denver Libraries",
+    },
+
+    repository: {
+      enableIIIFThumbnail: true,
+      enableIIIFItem: true,
+      fetchResourceFile: false,
+    },
+
+    search: {
+      
+    },
+  }
